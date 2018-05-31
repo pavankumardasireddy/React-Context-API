0d76f91de2b76d60e685b72078a18c1f66ee664c
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2018-05-31 14:16:58 +0530

    Upto:  storing  the data in provider and access it from consumer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//First we will make a new context
+const MyContext = React.createContext();
+
+//Create a provider component to store data
+class MyProvider extends Component {
+  state={
+    name:"Pavan kumar Dasireddy",
+    age:"23",
+    designation:"Fullstack Developer"
+  }
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state:this.state
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+
+const Family = (props) =>(
+  <div className="family">
+    <Person/>
+  </div>
+)
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Hey!! I'm Mr. {context.state.name}  </p>
+              <p>I'm a  {context.state.designation}  </p>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>        
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MyProvider>
+        <div>
+          <p>Hey! I'm App</p>
+          <Family/>
+        </div>
+      </MyProvider>      
     );
   }
 }
